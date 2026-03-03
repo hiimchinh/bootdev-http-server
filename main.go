358cb7b45b4f67aa585a4afcbd31ab8e97ffbbc2
hiimchinh <chinhhm@proton.me>
2026-03-03 23:37:55 +0700

    add healthz endpoint

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthCheckHandler)
 	server := &http.Server{Addr: ":8080", Handler: mux}
 	server.ListenAndServe()
 }
