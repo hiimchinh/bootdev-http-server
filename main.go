2aa00bd301fd848949aaba9cbb6f4f5a8a5c3d76
hiimchinh <chinhhm@proton.me>
2026-03-11 23:34:58 +0700

    add api validate chirp

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -41,6 +42,73 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "OK")
 }
 
+type parameters struct {
+	Body string `json:"body"`
+}
+
+type validRes struct {
+	Valid bool `json:"valid"`
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errRes struct {
+		Error string `json:"error"`
+	}
+	response := errRes{
+		Error: msg,
+	}
+
+	body, err := json.Marshal(response)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(body)
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	type validResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, 400, "Something went wrong")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(body)
+
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+
+		return
+	}
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+
+	}
+
+	valid := validRes{
+		Valid: true,
+	}
+	respondWithJSON(w, 200, valid)
+
+}
+
 func main() {
 	mux := http.NewServeMux()
 	config := &apiConfig{}
@@ -48,6 +116,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthCheckHandler)
 	mux.Handle("GET /admin/metrics", config.printHits())
 	mux.Handle("POST /admin/reset", config.resetHits())
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 	server := &http.Server{Addr: ":8080", Handler: mux}
 	server.ListenAndServe()
 }
